34e0c6198c1945ff63c84244cff06d5f57533314
Zaicu <>
2021-03-19 14:39:41 +0100

    Thomas prétraitement

--- pretraitement.py
+++ pretraitement.py
@@ -3,14 +3,14 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import numpy as np
-import json 
+import json
 
-xls = pd.ExcelFile('../product_data_1.xlsx')
-Products = pd.read_excel(xls, 'Products', skiprows=[0])
+xls = pd.ExcelFile('../socomec_chatbot/product_data_1.xlsx')
+Products = pd.read_excel(xls, 'Products')
 clas = Products.iloc[3,4]
 Class1 = pd.read_excel(xls, clas)
 
-stop_words = set(stopwords.words('french')) 
+stop_words = set(stopwords.words('french'))
 stop_words.add("quelle")
 stop_words.add("quel")
 stop_words.add("quels")
@@ -20,6 +20,8 @@ stop_words.add("!")
 stop_words.add("bonjour")
 stop_words.add("svp")
 stop_words.add("bonsoir")
+stop_words.add(".")
+stop_words.add(",")
 
 f = open('test_synonyms.json')
 synonymes = json.load(f)
@@ -29,66 +31,70 @@ synonymes = json.load(f)
 
 #fonction recherche d'appartenance d'un objet à une liste
 def is_in_list(object, list) :
-    n = len(list) 
-    i = 0 
-    while ((i<n)and(object != list[i])) :
-        i = i+1 
-    return(i<n) 
+	n = len(list)
+	i = 0
+	while ((i<n)and(object != list[i])) :
+		i = i+1
+	return(i<n)
 
+# filtre une phrase en renvoyant le tableau
+def filter_the_sentence(sentence):
+	sentence = sentence.replace("'", " ")
+	bag_of_words = word_tokenize(sentence)
 
+	filtered_sentence = []
+
+	for w in bag_of_words:
+		if w not in stop_words:
+			filtered_sentence.append(w)
+	return filtered_sentence
 
 #tokenisation, classification des termes significatifs de la question
 #grâce au dictionnaire des synonymes synonymes.json
 def question_treatement(question) :
-    question = question.replace("'", " ")
-    bag_of_words = word_tokenize(question)
+	filtered_sentence = filter_the_sentence(question)
 
-    filtered_sentence = [] 
-    word_class = []
+	print("filtered sentence :" , filtered_sentence)
 
-    for w in bag_of_words:  
-        if w not in stop_words:  
-            filtered_sentence.append(w) 
-    
-    print("filtered sentence :" , filtered_sentence)
+	word_class = []
 
-    for w in filtered_sentence :
-        print("word :", w) 
-        n = len(synonymes) 
-        i = 0
-        for key in synonymes : 
-            if (w.lower() == key.lower()) : 
-                word_class.append((w,synonymes[key]["class"]))
-                continue 
-            elif is_in_list(w, synonymes[key]["synonym"]) :
-                word_class.append((w,synonymes[key]["class"]))
-                continue
-            else :
-                i = i+1 
-        print ("i = ", i) 
-        if (i == n) : 
-            word_class.append((w, "Other")) 
-    
-    print(word_class)
-    
-    Product = ""
-    Feature = ""
-    for (w, c) in word_class : 
-        if (c == "Product") : 
-            Product += w 
-            Product += " "
-        
-        elif (c == "Feature") :
-            Feature += w 
-            Feature += " "
+	for w in filtered_sentence :
+		print("word :", w)
+		n = len(synonymes)
+		i = 0
+		for key in synonymes :
+			if (w.lower() == key.lower()) :
+				word_class.append((w,synonymes[key]["class"]))
+				continue
+			elif is_in_list(w, synonymes[key]["synonym"]) :
+				word_class.append((w,synonymes[key]["class"]))
+				continue
+			else :
+				i = i+1
+		print ("i = ", i)
+		if (i == n) :
+			word_class.append((w, "Other"))
 
-    return(Product, Feature) 
+	print(word_class)
+
+	Product = ""
+	Feature = ""
+	for (w, c) in word_class :
+		if (c == "Product") :
+			Product += w
+			Product += " "
+
+		elif (c == "Feature") :
+			Feature += w
+			Feature += " "
+
+	return(Product, Feature)
 
 
 
 #retourne le product id pour une ligne donnée
-def product_id(line) : 
-    return(Products['Product ID'][line])
+def product_id(line) :
+	return(Products['Product ID'][line])
 
 
 
@@ -97,50 +103,68 @@ def product_id(line) :
 #mesure de distance entre deux phrases en utilisant
 #la mesure de similarité de Jaccard.
 def distance_measurement(s1, s2) :
-    list1 = word_tokenize(s1)
-    list2 = word_tokenize(s2)
-    intersection = len(list(set(list1).intersection(list2)))
-    union = (len(list1) + len(list2)) - intersection
-    return (float(intersection) / union)
+	list1 = word_tokenize(s1)
+	list2 = word_tokenize(s2)
+	intersection = len(list(set(list1).intersection(list2)))
+	union = (len(list1) + len(list2)) - intersection
+	return (float(intersection) / union)
 
 
 
 
 #calcule la similarité entre le produit recherché
-#la description courte de la database. 
-def best_similarity(Product) : 
-    m = 0 
-    i = 0
-    l = i
-    best_line = ""
-    for line in Products['Description courte'] :
-        dist = distance_measurement(Product, str(line)) 
-        if dist > m : 
-            m = dist 
-            l = i 
-            best_line = str(line)
-        i = i+1 ; 
-    return(m, l, best_line)
+#la description courte de la database.
+def best_similarity(Product) :
+	m = 0
+	i = 0
+	l = i
+	best_line = ""
+	for line in Products['Description courte'] :
+		dist = distance_measurement(Product, str(line))
+		if dist > m :
+			m = dist
+			l = i
+			best_line = str(line)
+		i = i+1 ;
+	return(m, l, best_line)
 
+def set_product():
+	set_produit = set(['test'])
+	for ligne in range(1, 13677):
+		produit = Products['Description courte'][ligne]
+		if (produit == produit and produit != 0):
+			filtered_sentence = filter_the_sentence(produit)
+			set_produit = set_produit.union(set(filtered_sentence))
 
+		produit = Products['Description longue FR'][ligne]
+		if (produit == produit and produit != 0):
+			filtered_sentence = filter_the_sentence(produit)
+			set_produit = set_produit.union(set(filtered_sentence))
 
-def main() : 
-    phrase = "Quelle est la tension de fonctionnement du SIRCO 3x16 Ampères ?"
-    (P, F) = question_treatement(phrase) 
-    print("Produit :", P)
-    print("Feature :", F) 
-    (m, line, best_line) = best_similarity(P) 
-    print("best distance :", m) 
-    print("at line :" , line) 
-    print("description :", best_line)
-    print("product id :", product_id(line))
+	return set_produit
+
+def main() :
+	phrase = "Quelle est la tension de fonctionnement du SIRCO 3x16 Ampères ?"
+	(P, F) = question_treatement(phrase)
+	print("Produit :", P)
+	print("Feature :", F)
+	(m, line, best_line) = best_similarity(P)
+	print("best distance :", m)
+	print("at line :" , line)
+	print("description :", best_line)
+	print("product id :", product_id(line))
+
+	set_produit = set_product()
+	filtered_sentence = filter_the_sentence(phrase)
+	print(set_produit)
+	print(set_produit.intersection(set(filtered_sentence)))
 
 phrase2 = "Quelle est le voltage de l'interrupteur-sectionneur 3P 160A ?"
 
-(P, F) = question_treatement(phrase2) 
+(P, F) = question_treatement(phrase2)
 
-print("Produit :", P) 
-print("Feature :", F) 
+print("Produit :", P)
+print("Feature :", F)
 
-print("Execution du main :") 
-main() 
+print("Execution du main :")
+main()
--- synonyms_dictionary.py
+++ synonyms_dictionary.py
@@ -3,13 +3,13 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import numpy as np
-import json 
+import json
 
-xls = pd.ExcelFile('../product_data_1.xlsx')
-Products = pd.read_excel(xls, 'Products', skiprows=[0])
+xls = pd.ExcelFile('../socomec_chatbot/product_data_1.xlsx')
+Products = pd.read_excel(xls, 'Products')
 Sirco = pd.read_excel(xls, 'EC000216')
 Fuserbloc = pd.read_excel(xls, 'EC001040')
-Diris = pd.read_excel(xls, 'EC002301') 
+Diris = pd.read_excel(xls, 'EC002301')
 Countis = pd.read_excel(xls, 'EC001506')
 
 f = open('synonyms.json', "w")
@@ -19,12 +19,14 @@ n = Products.shape[0]
 
 dictionary_list = []
 
-for i in range (1,4) : 
+for i in range (1,4) :
     ec = Products.iloc[i, 4]
     if (ec == 'EC000216')or(ec == 'EC001040')or(ec == 'EC002301')or(ec == 'EC001506'):
         d = {}
         d["name"] = Products.iloc[i,1]
         d["class"] = "Product"
         d["synonyms"] = word_tokenize(d["name"])
-        dictionary_list.append(d) 
-        json.dump(dictionary_list, f, indent=4)
+        dictionary_list.append(d)
+json.dump(dictionary_list, f, indent=4)
+
+print(Sirco)
